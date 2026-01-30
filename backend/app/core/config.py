bcb3baf162fb81615396e8e5e0508628aebee3d6
Zer0F8th <25908759+Zer0F8th@users.noreply.github.com>
2026-01-29 20:16:43 -0500

    feat: initialize FastAPI application with CORS support and email parsing routes

---
+++ backend/app/core/config.py
@@ -0,0 +1,47 @@
+import secrets
+from typing import Annotated, Any, Literal
+from pydantic import (
+    AnyUrl,
+    BeforeValidator,
+    computed_field,
+)
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+# Ref: https://docs.pydantic.dev/latest/concepts/pydantic_settings/#usage
+# TODO: Update settings
+# TODO: This code is default pydantic settings example, modify as needed.
+# TODO: Update documentation
+
+
+def parse_cors(v: Any) -> list[str] | str:
+    if isinstance(v, str) and not v.startswith("["):
+        return [i.strip() for i in v.split(",") if i.strip()]
+    elif isinstance(v, list | str):
+        return v
+    raise ValueError(v)
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_file_encoding="utf-8",
+    )
+    API_V1_STR: str = "/api/v1"
+    SECRET_KEY: str = secrets.token_urlsafe(32)
+    ENVIRONMENT: Literal["local", "staging", "production"] = "local"
+    FRONTEND_HOST: str = "http://localhost:5173"
+    PROJECT_NAME: str = "My Awesome Project"
+    BACKEND_CORS_ORIGINS: Annotated[list[AnyUrl] | str, BeforeValidator(parse_cors)] = (
+        []
+    )
+
+    @computed_field  # type: ignore[prop-decorator]
+    @property
+    def all_cors_origins(self) -> list[str]:
+        return [str(origin).rstrip("/") for origin in self.BACKEND_CORS_ORIGINS] + [
+            self.FRONTEND_HOST
+        ]
+
+
+settings = Settings()  # type: ignore
